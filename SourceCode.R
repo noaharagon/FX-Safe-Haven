f1b4a17751571a205dcf2e31e54dc6b153c83899
noaharagon <50531441+noaharagon@users.noreply.github.com>
2021-03-25 13:29:56 +0100

    Update SourceCode.R

--- SourceCode.R
+++ SourceCode.R
@@ -1,3 +1,16 @@
-print("support the troops")
+#Jonas Schmitten & Noah Angara
+#Economics in Practice
+#March 2021
 
-print('MAGA 2024')
+#packages
+
+
+#setting working directory
+Paths = c("C://user/Fred/", "C://user/Wilma", "C://Some/other/path")
+names(Paths) = c("Fred", "Wilma", "Guest")
+setwd(Paths[Sys.info()[7]])
+
+
+# Data Cleaning -----------------------------------------------------------
+
+#load data
