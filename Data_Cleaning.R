5edc3f6f1a3062b7541f615bc5d0e235dfa2b0af
Jonista <46428455+Jonista@users.noreply.github.com>
2021-05-06 00:48:11 +0200

    Update

---
+++ Data_Cleaning.R
@@ -0,0 +1,184 @@
+#Jonas Schmitten & Noah Angara
+#Economics in Practice
+#March 2021
+
+#packages
+library("readxl")
+library("dplyr")
+library("purrr")
+library("stringr")
+library("xts")
+library("zoo")
+library("tibble")
+
+#setting working directory
+Paths = c("/Users/jonasschmitten/Desktop/FS 2021/Economics in Practice/Raw Data", 
+          "/Users/noahangara/Documents/Master's/8th Semester/Economics in Practice")
+names(Paths) = c("jonasschmitten", "noahangara")
+setwd(Paths[Sys.info()[7]])
+
+# Data Cleaning -----------------------------------------------------------
+
+#Load raw data
+spot_rates <- read_excel("FXData.xlsx", sheet='SpotRates')
+stable_coins <- read_excel("FXData.xlsx", sheet='StableCoins')
+independent <- read_excel("FXData.xlsx", sheet='IndependentVars')
+spreads <- read_excel("FXData.xlsx", sheet='Spreads')
+
+#Create DataFrame
+spot_rates <- as.data.frame(spot_rates)
+independent <- as.data.frame(independent)
+
+#Dates and Rearranging
+spot_rates[,seq(1, ncol(spot_rates), 2)] <- lapply(spot_rates[,seq(1, ncol(spot_rates), 2)], as.Date, format = "%y-%m-%d")
+
+#reverse order of some columns to make chronologically
+spot_rates[,23:ncol(spot_rates)] <- spot_rates[nrow(spot_rates):1, 23:ncol(spot_rates)]
+independent[,c(35:64,69:70)] <- independent[nrow(independent):1,c(35:64,69:70)]
+
+spot_rates = spot_rates[,1:22]
+
+# Function to clean data
+datacleanup <- function(data){
+  vars <- colnames(data[, seq(2, ncol(data), 2)])
+  df_list <- list()
+  for (i in vars) {
+    var <- paste("df", i, sep = ".")
+    assign(var, data[,(which(colnames(data)== i)-1):which(colnames(data)== i)], envir = .GlobalEnv)
+    df_list[[var]] <- var }
+  for (i in df_list){
+    d=get(i)
+    colnames(d)[1]<- "dates"
+    assign(i,d, envir = .GlobalEnv)}
+  assign(paste(deparse(substitute(data)),"_merged",sep=""), lapply(df_list, get) %>% reduce(full_join, by = "dates",na_matches="never"))
+}
+
+spot_rates <- datacleanup(spot_rates)
+
+rm(list=setdiff(ls(), c("spot_rates",'spreads','stable_coins','independent', "datacleanup")))
+
+#Crossrates
+for (i in c("JAPANESE YEN TO US $ (WMR) - EXCHANGE RATE",'NORWEGIAN KRONE TO US $ (WMR) - EXCHANGE RATE',
+            'BRAZILIAN REAL TO US $ (WMR) - EXCHANGE RATE',"INDIAN RUPEE TO US $ (WMR) - EXCHANGE RATE" )) { 
+  spot_rates[paste(str_split(i, "US",simplify = T)[1], "CHF (WMR) - EXCHANGE RATE", sep="")][1:which(grepl("2003-07-14", spot_rates$dates)),] = 
+    (1/spot_rates["SWISS FRANC TO US $ (WMR) - EXCHANGE RATE"][1:which(grepl("2003-07-14", spot_rates$dates)),])*
+    (spot_rates[i][1:which(grepl("2003-07-14", spot_rates$dates)),])}
+rm(i)
+spot_rates[,2:ncol(spot_rates)] = sapply(spot_rates[,2:ncol(spot_rates)], as.numeric)
+
+#fill in missing values with previous value
+#spot_rates <- na.locf(spot_rates)
+
+spreads = spreads[,1:29]
+
+#Calculating Bid-Ask Spreads
+spreads <- select(spreads, -5)
+spreads <- datacleanup(spreads)
+date_purposes <- spreads[1:5539,1]
+date_purposes <- as.Date(date_purposes$dates, format = "%Y-%m-%d")
+spreads <- spreads %>% select(-contains("Date"))
+spreads <- spreads %>% relocate("Ask Price CHF/INR", .after = "Bid Price CHF/INR")
+bid_ask <- spreads[, seq(2, ncol(spreads), 2)] - spreads[, seq(1, ncol(spreads), 2)]
+colnames(bid_ask)<-gsub("Ask Price","", colnames(bid_ask))
+bid_ask <- bid_ask[rowSums(is.na(bid_ask)) != ncol(bid_ask),]
+bid_ask$Date <- date_purposes
+bid_ask <- bid_ask %>% relocate("Date", .before = colnames(bid_ask)[1])
+
+bid_ask = bid_ask[5485:6,]
+bid_ask = na.locf(bid_ask)
+
+#compute log returns of spot rates
+#difflog <- function(x){
+#  diff(log(x))}
+
+diffsimple <- function(x){
+  diff(x)/(x[-length(x)])}
+
+#Take inverse to make CHF rates consistent
+spot_rates[c(5,7,9,11)] = 1/spot_rates[c(5,7,9,11)]
+#FX CHF/USD meaning how many CHF you can get for 1 USD, up means depreciate CHF (appreciate USD), down means appreciate CHF (depreciate USD)
+names(spot_rates) = c('dates', 'CHF/USD', 'CHF/GBP', 'CHF/EUR', 'CHF/JPY', 'JPY/USD', 'CHF/NOK', 'NOK/USD', 'CHF/BRL', 'BRL/USD', 'CHF/IDR', 'IDR/USD')
+
+spot_rates_returns <- as.data.frame(sapply(spot_rates[,2:ncol(spot_rates)], diffsimple))
+spot_rates_returns = add_column(spot_rates_returns, dates = spot_rates[2:nrow(spot_rates), "dates"] ,.before = 1)
+
+#split independent variables by time frequency
+daily_independent <- independent[,c(1:32,35:36,39:48,57:64)]
+weekly_independent <- independent[,c(37:38)]
+monthly_independent <- independent[,c(49:56,65:70)]
+
+#Replace Year Column of Geopolitical Risk with Date Column & Drop Months as Redundant & SNB Date Column
+monthly_independent[!is.na(monthly_independent[,9]), 9] <- rev(seq(as.Date("1997-01-01"), as.Date("2021-01-01"),by="months")-1)
+monthly_independent[!is.na(monthly_independent[,7]), 7] <-(seq(as.Date("2003-05-01"), as.Date("2021-04-01"),by="months")-1)
+monthly_independent[!is.na(monthly_independent[,13]), 13] <-(seq(as.Date("2000-02-01"), as.Date("2021-03-01"),by="months")-1)
+monthly_independent$Year...65 = sort(as.Date(monthly_independent$Year...65), na.last = T)
+monthly_independent <- monthly_independent[, c(1:9, 11, 13:14)]
+
+#Merge all daily independent variables into one DataFrame with uniform dates
+daily_independent <- datacleanup(daily_independent)
+daily_independent <- daily_independent[order(daily_independent$dates),]
+colnames(weekly_independent)[1] <- "dates"
+monthly_independent <- datacleanup(monthly_independent)
+monthly_independent <- monthly_independent[order(monthly_independent$dates),]
+
+#Remove Fund Flows
+monthly_independent <- select(monthly_independent, -5) 
+
+#monthly_independent <- datacleanup(monthly_independent)
+rm(list=setdiff(ls(),c('daily_independent', 'independent','spot_rates_returns', "spot_rates",
+                       'stable_coins', "datacleanup", "weekly_independent", "monthly_independent", "bid_ask", "diffsimple")))
+
+# Convert to numeric and Remove CDX & Last Price
+daily_independent[,2:ncol(daily_independent)] <- sapply(daily_independent[,2:ncol(daily_independent)], as.numeric)
+daily_independent <- select(daily_independent, -25)
+daily_independent <- select(daily_independent, -24)
+
+#Trim Barclay's US Corp High Yield pre 2000-03-17
+daily_independent <- daily_independent[3384:nrow(daily_independent), ]
+
+#removes rows where NAs across all columns (no date etc.) 
+daily_independent <- daily_independent[rowSums(is.na(daily_independent)) != ncol(daily_independent),]
+weekly_independent = weekly_independent[apply(weekly_independent,1,function(x)any(!is.na(x))),]
+monthly_independent <- monthly_independent[rowSums(is.na(monthly_independent)) != ncol(monthly_independent),]
+
+#Check maximum NAs in sequence, -INF means no more than 1 NA in a row 
+for (i in colnames(daily_independent)){
+  x = rle(is.na(daily_independent[,i]))
+  print(max(x$lengths[x$values][x$lengths[x$values] != max(x$lengths[x$values])]))}
+rm(x,i)
+
+daily_independent = na.locf(daily_independent, na.rm = F)
+
+#Create return datasets from independent variables
+daily_independent_returns = as.data.frame(sapply(daily_independent[2:ncol(daily_independent)], diffsimple))
+daily_independent_returns = add_column(daily_independent_returns, dates = daily_independent[2:nrow(daily_independent), "dates"] ,.before = 1)
+names(daily_independent_returns)[2:ncol(daily_independent_returns)] = c('MSCI', 'SPY', 'US_2Y', 'US_10Y', 'US_3M', 'DE_10Y','DE_3M', 'DE_2Y', 'PUT/CALL', 'VIX','TED_SPREAD', 'MOVE_1M', 
+                                    'MOVE_3M', 'MOVE_6M', 'VSTOXX', 'GOLD', 'CITI_ECONOMIC_SURPRISE', 'GS_COMMODITY_VOLA', 'JPM_GLOBAL_FX_VOLA', 'JPM_EM_FX_VOLA_1M',
+                                    'JPM_G10_FX_VOLA_1M', '10Y_BREAKEVEN', 'CDX_EUROPE_IG_10Y', 'BARC_US_CORP_HY_10Y')
+
+weekly_independent_returns = as.data.frame(sapply(weekly_independent, diffsimple))
+weekly_independent_returns$dates = weekly_independent[2:nrow(weekly_independent), "dates"]
+names(weekly_independent_returns) = c('dates', 'JPM_GLOBAL_FORECAST_REVISION_INDEX')
+
+#Adjust time frame to coincide with spot rates and daily independent variables
+monthly_independent = monthly_independent[which(grepl("2000-03-31", monthly_independent$dates)):nrow(monthly_independent),]
+monthly_independent_returns = as.data.frame(sapply(monthly_independent[2:ncol(monthly_independent)], diffsimple))
+monthly_independent_returns = add_column(monthly_independent_returns, dates = monthly_independent[2:nrow(monthly_independent), "dates"] ,.before = 1)
+
+names(monthly_independent_returns) = c('dates', 'IIF_EM_FLOWS', 'IIF_EM_DEBT_FLOWS', 'IIF_EM_EQUITY_FLOWS', 'GEOPOLITICAL_RISK', 'SNB_SIGHT_DEPOSITS')
+
+#Change path here for new folder where clean data is saved
+Paths = c("/Users/jonasschmitten/Desktop/FS 2021/Economics in Practice/Clean Data", 
+          "/Users/noahangara/Documents/Master's/8th Semester/Economics in Practice")
+names(Paths) = c("jonasschmitten", "noahangara")
+setwd(Paths[Sys.info()[7]])
+
+write.csv(bid_ask, 'Bid_Ask_Clean.csv', row.names = FALSE)
+write.csv(spot_rates, 'Spot_Rates_Clean.csv',row.names = FALSE)
+write.csv(spot_rates_returns, 'Spot_Rates_Returns_Clean.csv', row.names = FALSE)
+write.csv(daily_independent_returns, 'Daily_Independent_Returns_Clean.csv',row.names = FALSE)
+write.csv(weekly_independent_returns, 'Weekly_Independent_Returns_Clean.csv',row.names = FALSE)
+write.csv(monthly_independent_returns, 'Monthly_Independent_Returns_Clean.csv', row.names = FALSE)
+
+
+
---
+++ Data_Visualisation_and_Modelling.R
@@ -0,0 +1,103 @@
+#Jonas Schmitten & Noah Angara
+#Economics in Practice
+#March 2021
+
+#packages
+library("readxl")
+library("dplyr")
+library("purrr")
+library("stringr")
+library("xts")
+library("ggplot2")
+library("ggthemes")
+library("zoo")
+library("tibble")
+library("mixtools")
+library("stargazer")
+
+#setting working directory
+Paths = c("/Users/jonasschmitten/Desktop/FS 2021/Economics in Practice/Clean Data", 
+          "/Users/noahangara/Documents/Master's/8th Semester/Economics in Practice")
+names(Paths) = c("jonasschmitten", "noahangara")
+setwd(Paths[Sys.info()[7]])
+
+#Data 
+bid_ask = read.csv('Bid_Ask_Clean.csv')
+spot_rates = read.csv('Spot_Rates_Clean.csv')
+spot_rates_returns = read.csv('Spot_Rates_Returns_Clean.csv')
+daily_independent_returns = read.csv('Daily_Independent_Returns_Clean.csv')
+
+# Data Visualization ------------------------------------------------------
+
+#setting new working directory for graphs
+Paths = c("/Users/jonasschmitten/Desktop/FS 2021/Economics in Practice/Graphs", 
+          "/Users/noahangara/Documents/Master's/8th Semester/Economics in Practice")
+names(Paths) = c("jonasschmitten", "noahangara")
+setwd(Paths[Sys.info()[7]])
+
+# US Recession Start and End as per FRED St. Louis
+recessions.df = read.table(textConnection(
+  "Peak, Trough
+1973-11-01, 1975-03-01
+1980-01-01, 1980-07-01
+1981-07-01, 1982-11-01
+1990-07-01, 1991-03-01
+2001-03-01, 2001-11-01
+2007-12-01, 2009-06-01"), sep=',', colClasses=c('Date', 'Date'), header=TRUE)
+recessions.trim = subset(recessions.df, Peak >= "2000-03-17")
+rm(recessions.df)
+
+# Loop to Create Plots of Spot Rates With Recession Shading
+plot_list = list()
+for (i in colnames(spot_rates[2:ncol(spot_rates)])) {
+  z = zoo(spot_rates[, i], order.by = spot_rates$dates)
+  g = ggplot(fortify(z,melt=T)) +
+    geom_line(aes(x=Index,y=Value))+ 
+    geom_rect(data=recessions.trim, aes(xmin=Peak, xmax=Trough, ymin=-Inf, ymax=+Inf), fill='red', alpha=0.2)+
+    ylab("Spot Rate") +
+    xlab("Date") +
+    theme_economist_white() +
+    ggtitle(paste("Spot Rate of", i, sep = " "))
+  plot_list[[i]] = g
+  ggsave(g, file=paste0("plot_", i,".png"), width = 14, height = 10, units = "cm")
+}
+rm(c(i,z))
+
+
+# Data Exploration --------------------------------------------------------
+
+#summary statistics
+summary_stats <- summary(spot_rates)
+
+#histogram of FX returns
+for (i in colnames(spot_rates_returns[,2:ncol(spot_rates_returns)])) {
+  hist(spot_rates_returns[,i], breaks = 200, main = i)
+}
+
+#qq-plots to justify selection of distribution
+for (i in colnames(spot_rates_returns[,2:ncol(spot_rates_returns)])) {
+  qqnorm(spot_rates_returns[,i], pch = 1, frame = FALSE, main = i)
+  qqline(spot_rates_returns[,i], col = "steelblue", lwd = 2)
+}
+
+
+# Finite Gaussian Mixture  ------------------------------------------------
+
+CHF_reg <- regmixEM(y = spot_rates_returns[1:5479, 2], x = as.matrix(cbind(daily_independent_returns[1:5479, c(2, 3, 5, 6, 7)], bid_ask[1:5479,2])), k = 2)
+
+#for CHF/EURO take German yields instead of treasury?
+EUR_reg <- regmixEM(y = spot_rates_returns[1:5479, 3], x = as.matrix(cbind(daily_independent_returns[1:5479, c(2, 3, 4, 5, 6, 7)], bid_ask[1:5479,3])), k = 2)
+
+
+#plotting classification into "business as usual" and crisis
+EUR_mix <- normalmixEM(spot_rates_returns[1:5479, 3], k = 2)    
+EUR_mix_df <- spot_rates_returns[,c(3,12)]
+#if posterior of component 1 < 0.5 then component 2
+EUR_mix_df$component <- ifelse(EUR_mix$posterior[,1]<0.5, "2", "1")   
+ggplot(EUR_mix_df, aes(x = dates, y = EUR_mix_df[,1])) + 
+  geom_point(aes(colour = factor(component))) + theme_economist_white() + ggtitle("CHF/EUR: Business as Usual vs. Crisis") +
+  ylab("Spot Returns") + xlab("Date") 
+rm(EUR_mix_df)
+
+
+
